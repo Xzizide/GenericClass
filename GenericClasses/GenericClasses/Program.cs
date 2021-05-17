69b53b87d0f4c1345f6ea806ffa3543330e0b6ee
Xzizide <59053660+Xzizide@users.noreply.github.com>
2021-05-17 13:02:44 +0200

    Add files via upload

---
+++ GenericClasses/GenericClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GenericClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<string> stringClass = new GenericClass<string>(); //Skapar objekt med datatypen string
+            stringClass.Variable = "Hello"; //Sätter T till "Hello"
+            stringClass.WriteIt(stringClass.Variable); //Skriver ut T
+
+            GenericClass<int> intClass = new GenericClass<int>(); //Skapar objekt med datatypen int
+            intClass.WriteIt(5); //Skriver ut T
+        }
+    }
+
+    class GenericClass<T> //Generisk klass
+    {
+        public T Variable
+        {
+            get;
+            set;
+        }
+
+        public void WriteIt(T item)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
